2dc25857f8ea6a469f24a6d741f6d3d6e3ea2e33
Izhan <babuizhan1234@gmail.com>
2022-01-11 12:58:19 +0530

    Added image upload

--- components/Colorschemecard.tsx
+++ components/Colorschemecard.tsx
@@ -1,7 +1,56 @@
-const Colorschemecard = () => {
+import Image from "next/image";
+
+interface CardType {
+  cardData: {
+    title: string;
+    url: string;
+    colorscheme1: string;
+    colorscheme2: string;
+    colorscheme3: string;
+    colorscheme4: string;
+    colorscheme5: string;
+  };
+}
+
+const Colorschemecard = ({ cardData }: CardType) => {
   return (
-    <div>
-      <div></div>
+    <div className="mx-10 flex items-center justify-center flex-col">
+      <h3>{cardData.title}</h3>
+      <div>
+        {/* <Image src={`${cardData.url}`} alt={`${cardData.title}`} /> */}
+      </div>
+      <div className="grid grid-cols-5 ">
+        <div
+          className="p-10"
+          style={{
+            backgroundColor: `${cardData.colorscheme1}`,
+          }}
+        ></div>
+        <div
+          className="p-10"
+          style={{
+            backgroundColor: `${cardData.colorscheme2}`,
+          }}
+        ></div>
+        <div
+          className="p-10"
+          style={{
+            backgroundColor: `${cardData.colorscheme3}`,
+          }}
+        ></div>
+        <div
+          className="p-10"
+          style={{
+            backgroundColor: `${cardData.colorscheme4}`,
+          }}
+        ></div>
+        <div
+          className="p-10"
+          style={{
+            backgroundColor: `${cardData.colorscheme5}`,
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
--- pages/generate.tsx
+++ pages/generate.tsx
@@ -1,7 +1,56 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import fs from "fs";
+
 const Generate = () => {
+  const [file, setFile] = useState<File>();
+  const [title, setTitle] = useState<string>();
+  console.log(file);
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log(file);
+    const cloudName = "dm8ogh4lv";
+    const upload_preset = "w9skdyeo";
+
+    const formData = new FormData();
+    formData.append("file", file ? file : "");
+    formData.append("upload_preset", "w9skdyeo");
+
+    fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
+      method: "POST",
+      body: formData,
+    }).then((res) => {
+      axios
+        .post("http://localhost:3001/api/upload", {
+          body: {
+            title: title,
+            url: res.url,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+        });
+    });
+  };
+
   return (
     <div>
-      <div></div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          name="image"
+          id="image"
+          onChange={(e) => (e.target.files ? setFile(e.target.files[0]) : null)}
+        />
+        <input
+          type="text"
+          name="title"
+          id="title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
